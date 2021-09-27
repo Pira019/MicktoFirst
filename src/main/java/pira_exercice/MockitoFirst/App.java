035b4f1f3999a1784799e0d24c3a772a46eb4070
unknown <pires00243@gmail.com>
2021-09-27 18:55:49 +0100

    init

---
+++ src/main/java/pira_exercice/MockitoFirst/App.java
@@ -0,0 +1,13 @@
+package pira_exercice.MockitoFirst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
