e0f9b55509452e9a60361bb68a058184d1491cb8
unknown <pires00243@gmail.com>
2021-10-01 17:57:15 +0100

    Creating mock objects with the Mockito API

---
+++ src/test/java/pira_exercice/MockitoFirst/ServiceTest.java
@@ -0,0 +1,27 @@
+package pira_exercice.MockitoFirst;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ServiceTest {
+
+	@Mock
+    Database databaseMock;                                  
+
+    @Test
+    public void testQuery()  {
+        assertNotNull(databaseMock);
+        when(databaseMock.isAvailable()).thenReturn(true);  
+        Service t  = new Service(databaseMock);             
+        boolean check = t.query("* from t");                
+        assertTrue(check);
+    }
+
+}
